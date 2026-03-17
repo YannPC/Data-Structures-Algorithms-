f0ec3d590cf37a4952b550ee30ac0c1b54ac9119
YannMassoumou <YannPC>
2026-03-17 23:02:14 +0100

    completed the FInonacci sequence

---
+++ DSA_Project/Fibonacci-Sequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Project
+{
+    public class Fibonacci_Sequence
+    {
+        public void PrintSequence()
+        {
+            int numb1 = 0;
+            int numb2 = 1;
+
+            Console.WriteLine("Fibonacci Sequence: " + numb1);
+            Console.WriteLine("Fibonacci Sequence: " + numb2);
+
+            for (int i = 2; i < 10; i++)
+            {
+                int nextNumb = numb1 + numb2;
+                Console.WriteLine("Fibonacci Sequence: " + nextNumb);
+                numb1 = numb2;
+                numb2 = nextNumb;
+            }
+        }
+    }
+}
+
