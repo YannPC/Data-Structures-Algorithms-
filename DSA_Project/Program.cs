a57d0a15dfde63cbacbd9063e139d6347de2b406
YannMassoumou <YannPC>
2026-03-17 22:43:16 +0100

    Add project files.

---
+++ DSA_Project/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DSA_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb1 = 0;
+            int numb2 = 1;
+
+            Console.WriteLine("Fibonacci Sequence:" +numb1);
+            Console.WriteLine("Fibonacci Sequence:" +numb2);
+
+           for (int i = 2; i < 10; i++)
+            {
+                int nextNumb = numb1 + numb2;
+                Console.WriteLine("Fibonacci Sequence:" + nextNumb);
+                numb1 = numb2;
+                numb2 = nextNumb;
+            }
+        }
+    }
+}
