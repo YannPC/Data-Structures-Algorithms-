e289bb9fa47231b379c0444c212721534162e246
YannMassoumou <YannPC>
2026-04-20 21:35:40 +0200

    Sum of N number Given a positive integer n, find the sum of the first n natural numbers.
    Input: n = 3
    Output: 6
    Explanation: 1 + 2 + 3 = 6

---
+++ DSA_Project/SumOfN.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Project
+{
+    internal class SumOfN
+    {
+        public void SumOfNMethod()
+        {
+            Console.Write("Enter a positive integer: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"The sum of the first {n} natural numbers is: {sum}");
+        }
+    }
+}
