91a40db0d589a02a4b66d90d4b02d1fc4aaacd87
YannMassoumou <YannPC>
2026-05-16 23:45:18 +0200

    My Stack

---
+++ DSA_Project/myStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Project
+{
+    internal class myStack
+    {
+        private int[] array;
+
+        private int capacity;
+
+        private int top;
+
+        public myStack(int size)
+        {
+            
+            capacity = size;
+            array = new int[capacity];
+            top = -1;
+        }
+
+        public void push(int x)
+        {
+
+            if (top == capacity - 1)
+            {
+                Console.WriteLine("Stack Overflow");
+                return;
+            }
+
+            array[++top] = x;
+        }
+    }
+
+
+}
