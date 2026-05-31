b53c85e06882fb052e53274b17c219a3a3e5e0d0
YannMassoumou <YannPC>
2026-05-31 23:36:05 +0200

    arrange positive and negative number

---
+++ DSA_Project/Array_array_positive_negative_number.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Project
+{
+    internal class Array_array_positive_negative_number
+    {
+
+        public void SeparatePositiveNegative()
+        {
+            int[] arr = { -1, 2, -3, 4, -5, 6 };
+            int[] result = new int[arr.Length];
+            int posIndex = 0;
+            int negIndex = arr.Length - 1;
+            foreach (int num in arr)
+            {
+                if (num >= 0)
+                {
+                    result[posIndex++] = num;
+                }
+                else
+                {
+                    result[negIndex--] = num;
+                }
+            }
+            Console.WriteLine("Separated Array: " + string.Join(", ", result));
+        }
+    }
+}
